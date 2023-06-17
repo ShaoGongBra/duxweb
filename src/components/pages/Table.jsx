327fbc4a8c16c2aaa1d80cb95b2c92e85bef556b
xujian <908634674@qq.com>
2023-06-17 17:34:58 +0800

    新增參數

--- src/components/pages/Table.jsx
+++ src/components/pages/Table.jsx
@@ -1,57 +1,64 @@
-import React, {forwardRef, useImperativeHandle, useRef, useState} from 'react'
-import {PageList, Table as DuxTable} from '../../index'
+import React, { forwardRef, useImperativeHandle, useRef, useState, useMemo } from 'react'
+import { PageList, Table as DuxTable } from '../../index'
 
 export default forwardRef(({
-                             title, // 页面标题
-                             tabs, // 筛选切换
-                             menus, // 菜单栏
-                             search,
-                             filters, // 筛选元素
-                             defaultFilterData, // 筛选数据
-                             side, // 边栏
-                             columns, // 列配置
-                             url, // 数据地址
-                             data, // 数据
-                             limit = 20, // 每页数量
-                             primaryKey = 'id',
-                             width = 1000,
-                             permission, // 权限标记
-                             tableProps,
-                             dataCallBack, // 数据回调
-                             filterRender,
-                             header,
-                           }, ref) => {
+  title, // 页面标题
+  tabs, // 筛选切换
+  menus, // 菜单栏
+  search,
+  filters, // 筛选元素
+  defaultFilterData, // 筛选数据
+  side, // 边栏
+  columns, // 列配置
+  url, // 数据地址
+  data, // 数据
+  limit = 20, // 每页数量
+  primaryKey = 'id',
+  width = 1000,
+  permission, // 权限标记
+  tableProps,
+  dataCallBack, // 数据回调
+  filterRender,
+  header,
+  sortData
+}, ref) => {
 
-    const table = useRef(null)
-    useImperativeHandle(
-      ref,
-      () => ({
-        reload: () => table.current.reload(),
-      }),
-      []
-    )
+  const table = useRef(null)
+  useImperativeHandle(
+    ref,
+    () => ({
+      reload: () => table.current.reload(),
+    }),
+    []
+  )
 
-    const [tableData, setTableDat] = useState({})
 
-    return (
-      <PageList
-        title={title}
-        tabs={tabs}
-        menus={menus}
-        search={search}
-        tableData={tableData}
-        filters={filters}
-        defaultFilterData={defaultFilterData}
-        side={side}
-        permission={permission}
-        filterRender={filterRender}
-        header={header}
-      >
-        {([filterData]) => <DuxTable
+
+  const [tableData, setTableDat] = useState({})
+
+  return (
+    <PageList
+      title={title}
+      tabs={tabs}
+      menus={menus}
+      search={search}
+      tableData={tableData}
+      filters={filters}
+      defaultFilterData={defaultFilterData}
+      side={side}
+      permission={permission}
+      filterRender={filterRender}
+      header={header}
+    >
+      {([filterData]) => {
+        const requestData = useMemo(() => {
+          return { ...filterData, ...sortData }
+        }, [filterData, sortData])
+        return <DuxTable
           ref={table}
           columns={columns}
           url={url}
-          urlParams={filterData}
+          urlParams={requestData}
           data={data}
           limit={limit}
           primaryKey={primaryKey}
@@ -61,8 +68,9 @@ export default forwardRef(({
             setTableDat(e)
             dataCallBack?.(e)
           }}
-        />}
-      </PageList>
-    )
-  }
+        />
+      }}
+    </PageList>
+  )
+}
 )
