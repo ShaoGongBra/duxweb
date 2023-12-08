bb1105f8bbb6455f5e85d55ff4edfa430a0f3c59
duxweb <admin@dux.plus>
2023-12-08 16:00:54 +0800

    update

--- src/components/pages/List.jsx
+++ src/components/pages/List.jsx
@@ -68,7 +68,10 @@ export default function PageList(
                       return
                     }
                     return <div className='md:max-w-150'><Filter.Item key={key}
-                                                                      field={vo?.name}>{typeof vo?.render === 'function' ? vo?.render({tableData}) : vo?.render}</Filter.Item>
+                                                                      field={vo?.name}>{(itemFilter) => (typeof vo?.render === 'function' ? vo?.render({
+                      tableData,
+                      itemFilter
+                    }) : vo?.render)}</Filter.Item>
                     </div>
                   })
                 }
